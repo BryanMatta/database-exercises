15f04686e477a251d44301b438be38bc8d9d1c3d
Bryan Matta <bryan.m.matta@gmail.com>
2019-01-30 13:54:13 -0600

    finished aliases

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,9 +1,23 @@
 use employees;
 select distinct title from titles;
-select distinct last_name from employees where last_name like 'E%e'
+
+select last_name
+from employees
+where last_name like 'E%e'
 group by last_name;
-select distinct last_name from employees where last_name like '%q%'
-and last_name not like'%qu%';
+
+select last_name, first_name
+from employees
+where last_name like 'e%e'
+group by last_name, first_name;
+
+select last_name, count(last_name)
+from employees
+where last_name like '%q%'
+and last_name not like'%qu%'
+group by last_name
+order by last_name;
+
 select count(*) as Employees,gender from employees where first_name = 'Irena'
 or first_name = 'Vidya'
 or first_name = 'Maya'
